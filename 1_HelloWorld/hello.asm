4c91cdb07578318094896f208296ba336c766ecd
shaheemMPM <shaheem.mpm@gmail.com>
2020-01-27 21:01:35 +0530

    Comment Alignment

--- 1_HelloWorld/hello.asm
+++ 1_HelloWorld/hello.asm
@@ -1,16 +1,16 @@
-        SECTION .data          ; data section
-msg:    db "Hello World",10        ; the string to print, 10=cr
-len:    equ $-msg          ; "$" means "here"
+        SECTION .data           ; data section
+msg:    db "Hello World",10     ; the string to print, 10=cr
+len:    equ $-msg               ; "$" means "here"
                                 ; len is a value, not an address
 
-        SECTION .text          ; code section
+        SECTION .text           ; code section
         global main             ; make label available to linker as invoked by gcc
         global _start           ; make label available to linker w/ default entry
 main:                           ; standard  gcc  entry point
 _start:
 
-        mov    edx,len            ; arg3, length of string to print
-        mov    ecx,msg            ; arg2, pointer to string
+        mov    edx,len          ; arg3, length of string to print
+        mov    ecx,msg          ; arg2, pointer to string
         mov    ebx,1
         mov    eax,4
         int    0x80
--- 2_Addition_with_hard_coded_integers/addition.asm
+++ 2_Addition_with_hard_coded_integers/addition.asm
@@ -1,6 +1,6 @@
 section .text
-    global _start    ;must be declared for using gcc
-_start:    ;tell linker entry point
+    global _start              ;must be declared for using gcc
+_start:                        ;tell linker entry point
     mov     eax,'3'
     sub     eax, '0'
     mov     ebx, '4'
@@ -10,16 +10,16 @@ _start:    ;tell linker entry point
     mov     [sum], eax
     mov ecx,msg
     mov edx, len
-    mov ebx,1   ;file descriptor (stdout)
-    mov eax,4   ;system call number (sys_write)
-    int 0x80    ;call kernel
+    mov ebx,1                  ;file descriptor (stdout)
+    mov eax,4                  ;system call number (sys_write)
+    int 0x80                   ;call kernel
     mov ecx,sum
     mov edx, 1
-    mov ebx,1   ;file descriptor (stdout)
-    mov eax,4   ;system call number (sys_write)
-    int 0x80    ;call kernel
-    mov eax,1   ;system call number (sys_exit)
-    int 0x80    ;call kernel
+    mov ebx,1                  ;file descriptor (stdout)
+    mov eax,4                  ;system call number (sys_write)
+    int 0x80                   ;call kernel
+    mov eax,1                  ;system call number (sys_exit)
+    int 0x80                   ;call kernel
 
 section .data
     msg db "The sum is:", 0xA,0xD
